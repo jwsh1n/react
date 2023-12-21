a257773a5004715aa86a0011f7c750d441dd983b
jwshin <singee98@naver.com>
2023-12-21 17:32:28 +0900

    react 6장 수정 전

---
+++ src/chapter_06/notification.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const styles = {
+  wrapper: {
+    margin: 8,
+    padding: 8,
+    dispaly: "flex",
+    flexDirection: "row",
+    border: "1px solid grey",
+  }
+}
